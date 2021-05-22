2c6058448bf79fcd5bd304fffb519ab0f41903cc
IVAN <aguas.xavier2013a@gmail.com>
2021-05-21 22:07:12 -0500

    Add libraries

--- FirstSteps.py
+++ FirstSteps.py
@@ -6,3 +6,9 @@ Created on Fri May 21 21:54:07 2021
 @author: xavieraguas
 """
 
+from MovieLens import MovieLens
+from surprise import SVD
+from surprise import KNNBaseline
+from surprise.model_selection import train_test_split
+from surprise.model_selection import LeaveOneOut
+from RecommenderMetrics import RecommenderMetrics
