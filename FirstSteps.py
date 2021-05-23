7fd63f149bf4f9e98d6b08545a4579e1c5582e85
IVAN <aguas.xavier2013a@gmail.com>
2021-05-22 20:01:16 -0500

    predictions

--- FirstSteps.py
+++ FirstSteps.py
@@ -28,3 +28,14 @@ fullTrainSet = data.build_full_trainset()
 sim_options = {'name': 'pearson_baseline', 'user_based': False}   # compute  similarities between items
 simsAlgo = KNNBaseline(sim_options=sim_options)
 simsAlgo.fit(fullTrainSet)
+
+
+# Construccion del sistema de recomendacion
+# 75% Training 25% testing
+trainSet, testSet = train_test_split(data, test_size=.25, random_state=1)
+algo = SVD(random_state=10)
+algo.fit(trainSet)
+
+
+#Prediciones
+predictions = algo.test(testSet)
