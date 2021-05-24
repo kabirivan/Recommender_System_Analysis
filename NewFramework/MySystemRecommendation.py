1cc9c51c490c25b5034a8fd9545ff66967644e9d
IVAN <aguas.xavier2013a@gmail.com>
2021-05-24 12:04:41 -0500

    Add libraries

---
+++ NewFramework/MySystemRecommendation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 11:47:05 2021
+
+@author: xavieraguas
+"""
+
+from MovieLens import MovieLens
+from surprise import SVD
+from surprise import NormalPredictor
+from Evaluator import Evaluator
+
+
+import random
+import numpy as np
+
+
+def LoadMovieLensData():
+    ml = MovieLens()
+    print("Carga de ratings de las peliculas...")
+    data = ml.loadMovieLensLatestSmall()
+    print("\nCalculo de las peliculas que tienen mas calificaciones --> Novedad...")
+    rankings = ml.getPopularityRanks()
+    return (data, rankings)
+
+
+np.random.seed(0)
+random.seed(0)
